d3832f7fc64cad7d640885bd54210347bb658c4f
Aliko <alsydm091@gmail.com>
2021-07-18 23:15:54 +0300

    295

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:time_tracker/app/home/jobs_page.dart';
+import 'package:time_tracker/app/home/jobs/jobs_page.dart';
 import 'package:time_tracker/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker/services/auth.dart';
 import 'package:time_tracker/services/database.dart';
